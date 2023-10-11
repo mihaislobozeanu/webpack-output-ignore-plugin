91f74f8b52468e645bb7f9082e5b221356ca5626
Mihai Slobozeanu <mihai@antebit.com>
2023-10-11 13:29:18 +0300

    webpack 5 compatibility

--- index.js
+++ index.js
@@ -2,23 +2,22 @@
  * Created by mihai on 4/30/2016.
  */
 
-var ModuleFilenameHelpers = require('webpack/lib/ModuleFilenameHelpers');
+const ModuleFilenameHelpers = require('webpack/lib/ModuleFilenameHelpers');
 
-function OutputIgnorePlugin(options){
+function OutputIgnorePlugin(options) {
 	this.options = options || {};
 }
 
 module.exports = OutputIgnorePlugin;
 
-OutputIgnorePlugin.prototype.apply = function(compiler){
-	var self = this,
+OutputIgnorePlugin.prototype.apply = function (compiler) {
+	const self = this,
 		options = self.options;
 
-	compiler.plugin('emit', function(compilation, callback){
-		var assets = Object.keys(compilation.assets).filter(ModuleFilenameHelpers.matchObject.bind(undefined, options));
-		assets.forEach(function(asset){
+	compiler.hooks.emit.tap('OutputIgnorePlugin', (compilation) => {
+		const assets = Object.keys(compilation.assets).filter(ModuleFilenameHelpers.matchObject.bind(undefined, options));
+		assets.forEach(function (asset) {
 			delete compilation.assets[asset];
 		});
-		callback();
 	});
 };
